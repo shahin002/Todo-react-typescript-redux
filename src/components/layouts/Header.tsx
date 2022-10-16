3ee0c201166100511bdb44e4cffb27ea0aa84b06
Md Shahin Howlader <mdshahin0002@gmail.com>
2022-10-17 00:12:17 +0200

    Add Layout Header

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Nav, Navbar} from "react-bootstrap";
+import {Link} from "react-router-dom";
 
 const Header = () => {
+    const [currentUser, setCurrentUser] = useState(false);
+    // @ts-ignore
+    const [userData, setUserData] = useState(JSON.parse(localStorage.getItem("userData")) || undefined);
+
+    useEffect(() => {
+        if (typeof userData != "undefined") {
+            setCurrentUser(true);
+        } else {
+            setCurrentUser(false);
+        }
+    }, [currentUser]);
     return (
-        <div>
-            This is header
-        </div>
+        <Navbar className="color-nav" variant="light"expand="lg" >
+            <div className="container">
+                <Navbar.Brand href="/">
+                    Welcome to Board <b>{userData.user.displayName}</b>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav >
+                        {currentUser && (
+                            <>
+                                <Nav.Link>
+                                    <Link to="/">Home</Link>
+                                </Nav.Link>
+
+                                <Nav.Link>
+                                    {/*<Link onClick={signOutUser} to={""}>*/}
+                                    {/*    {" "}*/}
+                                    {/*    SignOut*/}
+                                    {/*</Link>*/}
+                                </Nav.Link>
+                            </>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
+            </div>
+        </Navbar>
     );
 };
 
