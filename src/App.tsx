a12e5517f4ef63672a82881579cdedb61ef8f34d
Md Shahin Howlader <mdshahin0002@gmail.com>
2022-10-16 22:31:23 +0200

    Add Login page and TaskList page

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import TaskListPage from "./views/TaskListPage";
+import LoginPage from "./views/LoginPage";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(false);
+  useEffect(() => {
+    // @ts-ignore
+    const userData = JSON.parse(localStorage.getItem("userData")) || undefined;
+    console.log(userData)
+    if (typeof userData !== 'undefined') {
+      setCurrentUser(true);
+    }else{
+      setCurrentUser(false)
+    }
+  }, [currentUser]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <BrowserRouter>
+        <Routes>
+
+
+          <Route  path="/" element={
+            currentUser?<TaskListPage/>:<Navigate to="/login" />
+          } />
+
+          <Route  path="/login" element={
+            currentUser?<Navigate to="/" />:<LoginPage/>
+          } />
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/views/TaskListPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TaskListPage = () => {
+    return (
+        <div>
+            This is task list page
+        </div>
+    );
+};
+
+export default TaskListPage;
